55d4d5c3d8bf61045f60e6bfed9dbf0883fe3355
ThiagoH03 <thiagohenriq03@outlook.com>
2022-06-06 17:38:18 +0000

    Vários números foram impressos

---
+++ Numeros.java
@@ -0,0 +1,7 @@
+public class Numeros{
+    public static void main(String[] args) {
+        for(int numeros = 38; numeros <= 1000; numeros++){
+            System.out.print("[" + numeros + "] ");
+        }
+    }
+}
